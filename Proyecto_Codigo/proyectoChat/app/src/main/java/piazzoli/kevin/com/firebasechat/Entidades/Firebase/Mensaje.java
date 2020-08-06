0174c13efd8e3d1bc2e55fd00f37a81346c0c931
odyt2112 <odyt_17@hotmail.com>
2020-08-06 14:48:56 -0500

    Codigo Unido

---
+++ Proyecto_Codigo/proyectoChat/app/src/main/java/piazzoli/kevin/com/firebasechat/Entidades/Firebase/Mensaje.java
@@ -0,0 +1,60 @@
+package piazzoli.kevin.com.firebasechat.Entidades.Firebase;
+
+import com.google.firebase.database.ServerValue;
+
+/**
+ * Creado por
+ * Nicol Arias
+ * Nathali Fernandez
+ * Oscar Taco
+ * Materia: Programacion Movil
+ */
+
+public class Mensaje {
+
+    private String mensaje;
+    private String urlFoto;
+    private boolean contieneFoto;
+    private String keyEmisor;
+    private Object createdTimestamp;
+
+    public Mensaje() {
+        createdTimestamp = ServerValue.TIMESTAMP;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public boolean isContieneFoto() {
+        return contieneFoto;
+    }
+
+    public void setContieneFoto(boolean contieneFoto) {
+        this.contieneFoto = contieneFoto;
+    }
+
+    public String getKeyEmisor() {
+        return keyEmisor;
+    }
+
+    public void setKeyEmisor(String keyEmisor) {
+        this.keyEmisor = keyEmisor;
+    }
+
+    public Object getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+}
